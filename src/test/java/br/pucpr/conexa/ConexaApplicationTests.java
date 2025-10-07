a4e53780c203dd0081db3fe6a8c1d7684f458611
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-10-07 12:30:30 -0300

    :tada: first commit

---
+++ src/test/java/br/pucpr/conexa/ConexaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucpr.conexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
